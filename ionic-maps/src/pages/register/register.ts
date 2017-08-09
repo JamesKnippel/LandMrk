fadb33b547c5f9887f4fcd29f15ea1fbf0fd0a30
Mike Doyle <mikedoyle007@gmail.com>
2017-08-08 17:18:29 -0700

    Handle register functionality

--- ionic-maps/src/pages/register/register.ts
+++ ionic-maps/src/pages/register/register.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Account } from '../../models/account/account.interface';
+import { ToastController } from 'ionic-angular';
 
 /**
  * Generated class for the RegisterPage page.
@@ -15,11 +18,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegisterPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  account = {} as Account;
+
+  constructor(private toast: ToastController, private afAuth: AngularFireAuth) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
+  async register() {
+    try {
+      const result = await this.afAuth.auth.createUserWithEmailAndPassword(this.account.email, this.account.password);
+      this.toast.create({
+        message: 'Account successfully created!',
+        duration: 3000
+      }).present();
+      console.log(result);
+    }
+    catch(e) {
+      console.error(e);
+      this.toast.create({
+        message: e.message,
+        duration: 3000
+      }).present();
+    }
   }
-
 }
