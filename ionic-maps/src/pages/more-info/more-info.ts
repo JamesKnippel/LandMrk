8c1ef5ad41af3ca4530b01d9c6639657a23b0b50
tonymichaelhead <tonymichaelhead@gmail.com>
2017-08-11 13:53:31 -0700

    Render wiki data to more info page

--- ionic-maps/src/pages/more-info/more-info.ts
+++ ionic-maps/src/pages/more-info/more-info.ts
@@ -32,14 +32,12 @@ export class MoreInfoPage {
 
   getWikiInformation():void {
     //TODO: Change data type to conform to wiki model interface
-    //TODO: Change 'pizza' test wikiTopic to be input dynamically
     //TODO: Add wiki url that opens wikipedia in the browser
-    this.wiki.getWiki('pizza').subscribe((data: Wiki) => {
+    this.wiki.getWiki('landmark').subscribe((data: Wiki) => {
       this.wikiDummyData[1][0] = data[1][0];
       this.wikiDummyData[2][0] = data[2][0];
     
     });
-    //console.log('more info: ', data));
   }
 
   ionViewDidLoad() {
